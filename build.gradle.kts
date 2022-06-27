6dfde41dbefb46d95ccab0b56ba07bf379e97ca3
Roberto Casadei <robyonrails@gmail.com>
2022-06-27 20:02:10 +0200

    Restore their gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,64 @@
 import de.aaschmid.gradle.plugins.cpd.Cpd
 
 plugins {
+    // Apply the java plugin to add support for Java
     java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+
     alias(libs.plugins.java.qa)
     alias(libs.plugins.taskTree)
 }
 
-sourceSets {
-    main {
-        java {
-            srcDirs("src")
-        }
-        resources {
-            srcDirs("res")
-            srcDirs("resources")
-        }
-    }
-    test {
-        java {
-            srcDirs("src")
-        }
-        resources {
-            srcDirs("res")
-            srcDirs("resources")
-        }
-    }
-}
-
 repositories {
     mavenCentral()
-    flatDir {
-        dirs("lib")
-    }
 }
 
+
+val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
+
+
+val jUnitVersion = "5.7.1"
+
 dependencies {
-    implementation(libs.bundles.junit.compile)
-    runtimeOnly(libs.bundles.junit.runtime)
-    File("lib")
-        .takeIf { it.exists() }
-        ?.takeIf { it.isDirectory }
-        ?.listFiles()
-        ?.filter { it.extension == "jar" }
-        ?.forEach { implementation(files("lib/${it.name}")) }
-    implementation("com.google.code.gson:gson:2.9.0")
+    // Example library: Guava. Add what you need (and remove Guava if you don't use it)
+    // implementation("com.google.guava:guava:28.1-jre")
+    implementation ("com.google.code.gson:gson:2.8.6")
+
+    // JUnit API and testing engine
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
 }
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Test> {
+    // Enables JUnit 5 Jupiter module
+    useJUnitPlatform()
+}
+
+
+application {
+    // Define the main class for the application
+    mainClass.set("it.aren.App")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
 
 allprojects {
     tasks.withType<Test> {
